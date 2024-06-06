bcc9d25615009b122d05292a41fb310205481d48
Latoya <76912011+latoyaln@users.noreply.github.com>
2024-06-06 20:43:47 +0200

    base of project

--- server.js
+++ server.js
@@ -1,1 +1,54 @@
-console.log('Hier komt je server voor Sprint 12.')
+// Importeer het npm pakket express uit de node_modules map
+import express from 'express'
+
+// Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
+import fetchJson from './helpers/fetch-json.js'
+
+import { AwesomeGraphQLClient } from 'awesome-graphql-client'
+// Maak een nieuwe express app aan
+const app = express();
+
+// Stel ejs in als template engine
+app.set('view engine', 'ejs');
+
+// Stel de map met ejs templates in
+app.set('views', './views');
+
+// Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
+app.use(express.static('public'));
+
+// Zorg dat werken met request data makkelijker wordt
+app.use(express.urlencoded({extended: true}));
+
+// API naar hygraph
+
+const client = new AwesomeGraphQLClient({
+  endpoint:
+    'https://eu-central-1-shared-euc1-02.cdn.hygraph.com/content/clvw76z7t00aq07uzaijft13n/master',
+  fetch,
+});
+
+app.get('/', async (request, response) => {
+  const query = `
+  { 
+    teams {
+      name
+      slug
+    }
+  }
+`;
+
+const { teams } = await client.request(query);
+
+response.render('index', { teams });
+});
+
+// Stel het poortnummer in waar express op moet gaan luisteren
+app.set('port', process.env.PORT || 8000);
+
+// Start express op, haal daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get('port'), function() {
+  // Toon een bericht in de console en geef het poortnummer door
+  console.log(`Application started on http://localhost:${app.get('port')}`);
+});
+
