627c35c8b33e7a3eef83f4cf80ba69a710ca95b0
Latoya <76912011+latoyaln@users.noreply.github.com>
2024-06-12 08:41:56 +0200

    dashboard data added

--- server.js
+++ server.js
@@ -1,66 +1,58 @@
-// Importeer het npm pakket express uit de node_modules map
-import express from 'express'
+import express from 'express';
+import fetchJson from './helpers/fetch-json.js';
+import { AwesomeGraphQLClient } from 'awesome-graphql-client';
 
-// Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
-import fetchJson from './helpers/fetch-json.js'
-
-import { AwesomeGraphQLClient } from 'awesome-graphql-client'
-// Maak een nieuwe express app aan
 const app = express();
 
-// Stel ejs in als template engine
-app.set('view engine', 'ejs');
-
-// Stel de map met ejs templates in
-app.set('views', './views');
-
-// Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
+// Serve static files from the 'public' directory
 app.use(express.static('public'));
 
-// Zorg dat werken met request data makkelijker wordt
-app.use(express.urlencoded({extended: true}));
+// Set up EJS as the template engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
-// API naar hygraph
+// Handle URL-encoded data
+app.use(express.urlencoded({ extended: true }));
 
 const client = new AwesomeGraphQLClient({
-  endpoint:
-    'https://eu-central-1-shared-euc1-02.cdn.hygraph.com/content/clvw76z7t00aq07uzaijft13n/master',
+  endpoint: 'https://eu-central-1-shared-euc1-02.cdn.hygraph.com/content/clvw76z7t00aq07uzaijft13n/master',
   fetch,
 });
 
 app.get('/', async (request, response) => {
-  const team = `
-  { 
-    teams {
-      name
-      slug
+  const teamQuery = `
+    { 
+      teams {
+        name
+        slug
+      }
     }
-  }
-`;
+  `;
 
-const employee = `
-{
-  employees {
-    name
-    age
-    role
-   avatar {
-     url
-   }
-   team{
-    name
-   }
-  }
-}`;
+  const employeeQuery = `
+    {
+      employees {
+        name
+        age
+        role
+        avatar {
+          url
+        }
+        team {
+          name
+        }
+      }
+    }
+  `;
 
-const { teams } = await client.request(team);
-const { employees } = await client.request(employee);
+  const { teams } = await client.request(teamQuery);
+  const { employees } = await client.request(employeeQuery);
 
-response.render('index', { teams, employees});
+  response.render('index', { teams, employees });
 });
 
 app.get('/team/:slug', async function (req, res) {
-  const team = `
+  const teamQuery = `
     query TeamsPageQuery($slug: String!) {
       teams(where: { slug: $slug }) {
         name
@@ -79,17 +71,21 @@ app.get('/team/:slug', async function (req, res) {
 
   const { slug } = req.params;
 
-  const {teams} = await client.request(team, { slug });
-  console.log({teams});
-  res.render('teams', {teams});
+  try {
+    const { teams } = await client.request(teamQuery, { slug });
+
+    if (teams.length > 0) {
+      res.render('teams', { teams });
+    } else {
+      res.render('teams', { teams: [] });
+    }
+  } catch (error) {
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-// Stel het poortnummer in waar express op moet gaan luisteren
 app.set('port', process.env.PORT || 8000);
 
-// Start express op, haal daarbij het zojuist ingestelde poortnummer op
 app.listen(app.get('port'), function() {
-  // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get('port')}`);
 });
-
